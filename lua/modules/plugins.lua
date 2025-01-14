264fe14519552f85002613e4a2893c128b414411
Rahul <rahul.j.jacob@gmail.com>
2025-01-14 11:16:48 +0530

    snacks

--- lua/modules/plugins.lua
+++ lua/modules/plugins.lua
@@ -1,4 +1,4 @@
-local snacks_setup = require "modules.snacks_setup"
+local snacks_config = require "modules.snacks_config"
 --Setup Lazy
 local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
@@ -147,55 +147,8 @@ require("lazy").setup({
     priority = 1000,
     lazy = false,
     ---@type snacks.Config
-    opts = {
-      bigfile = { enabled = true },
-      dashboard = { enabled = true },
-      indent = { enabled = false },
-      input = { enabled = true },
-      notifier = {
-        enabled = true,
-        timeout = 3000,
-      },
-      quickfile = { enabled = true },
-      scroll = { enabled = true },
-      statuscolumn = { enabled = true },
-      words = { enabled = true },
-      styles = {
-        notification = {
-          -- wo = { wrap = true } -- Wrap notifications
-        },
-      },
-    },
-    -- keys = snacks_setup.keys,
-    init = function()
-      vim.api.nvim_create_autocmd("User", {
-        pattern = "VeryLazy",
-        callback = function()
-          -- Setup some globals for debugging (lazy-loaded)
-          _G.dd = function(...)
-            Snacks.debug.inspect(...)
-          end
-          _G.bt = function()
-            Snacks.debug.backtrace()
-          end
-          vim.print = _G.dd -- Override print to use snacks for `:=` command
-
-          -- Create some toggle mappings
-          Snacks.toggle.option("spell", { name = "Spelling" }):map "<leader>us"
-          Snacks.toggle.option("wrap", { name = "Wrap" }):map "<leader>uw"
-          Snacks.toggle.option("relativenumber", { name = "Relative Number" }):map "<leader>uL"
-          Snacks.toggle.diagnostics():map "<leader>ud"
-          Snacks.toggle.line_number():map "<leader>ul"
-          Snacks.toggle
-            .option("conceallevel", { off = 0, on = vim.o.conceallevel > 0 and vim.o.conceallevel or 2 })
-            :map "<leader>uc"
-          Snacks.toggle.treesitter():map "<leader>uT"
-          Snacks.toggle.option("background", { off = "light", on = "dark", name = "Dark Background" }):map "<leader>ub"
-          Snacks.toggle.inlay_hints():map "<leader>uh"
-          Snacks.toggle.indent():map "<leader>ug"
-          Snacks.toggle.dim():map "<leader>uD"
-        end,
-      })
-    end,
+    opts = snacks_config.opts(),
+    keys = snacks_config.keys(),
+    -- init = snacks_config.init()
   },
 }, {})
