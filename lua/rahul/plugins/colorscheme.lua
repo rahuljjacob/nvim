0c07cd701e57f9f8f994569c326ebeb4e1c86a5c
Rahul <rahul.j.jacob@gmail.com>
2025-07-28 12:19:39 +0530

    colorscheme

--- lua/rahul/plugins/colorscheme.lua
+++ lua/rahul/plugins/colorscheme.lua
@@ -1,13 +1,22 @@
+-- return {
+-- 	{
+-- 		"folke/tokyonight.nvim",
+-- 		priority = 1000, -- make sure to load this before all the other start plugins
+-- 		config = function()
+-- 			require("tokyonight").setup({
+-- 				style = "night",
+-- 			})
+-- 			-- load the colorscheme here
+-- 			vim.cmd([[colorscheme tokyonight]])
+-- 		end,
+-- 	},
+-- }
+--
 return {
-	{
-		"folke/tokyonight.nvim",
-		priority = 1000, -- make sure to load this before all the other start plugins
-		config = function()
-			require("tokyonight").setup({
-				style = "night",
-			})
-			-- load the colorscheme here
-			vim.cmd([[colorscheme tokyonight]])
-		end,
-	},
+	"nyoom-engineering/oxocarbon.nvim",
+	priority = 1000, -- make sure to load this before all the other start plugins
+	config = function()
+		vim.opt.background = "dark" -- set this to dark or light
+		vim.cmd("colorscheme oxocarbon")
+	end,
 }
